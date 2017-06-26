f86e20e10eb4d7864431933012efa88f8829fe8a
Alex Reany <reanyalex@gmail.com>
2017-06-25 17:39:22 -0700

    fixed a few gameplay bugs

--- js/blackJackInterface.js
+++ js/blackJackInterface.js
@@ -1,37 +1,38 @@
 const i = new Interface()
-const suite = ["heart","spades","clubs","diamonds"];
+const suite = ["heart", "spades", "clubs", "diamonds"];
 function Interface() {
   this.deckArray = [];
   this.deck = new Deck
   this.player1 = new Player(true)
   this.player2 = new Player()
   this.bet = 0;
+  this.show = false;
 }
 
-Interface.prototype.shuffle = function () {
+Interface.prototype.shuffle = function() {
   //same keys for all the suits
   const deckKeys = Object.keys(this.deck.heart)
   let self = this
   deckKeys.map(function(card) {
     //iterates over suite to find suite info
     for (var j = 0; j <= 3; j++) {
-    self.deckArray.push(self.deck[suite[j]][card])
+      self.deckArray.push(self.deck[suite[j]][card])
     }
   })
   i.deal()
   i.cardValue()
 };
 
-Interface.prototype.deal = function () {
+Interface.prototype.deal = function() {
   for (let i = 0; i < 4; i++) {
     let random = this.selectCard()
-    if (i%2 === 0) {
+    if (i % 2 === 0) {
       //to deal the two cards use the hit function twice for each player
       this.hit()
       //changes the turn of the player only here for deal will have stay change turn
       this.player1.turn = !this.player1.turn
       this.player2.turn = !this.player2.turn
-    }else {
+    } else {
       this.hit()
       //changes the turn of the player only here for deal will have stay change turn
       this.player1.turn = !this.player1.turn
@@ -40,7 +41,7 @@ Interface.prototype.deal = function () {
   }
 };
 
-Interface.prototype.cardValue = function () {
+Interface.prototype.cardValue = function() {
   let length1 = this.player1.cards.length
   let length2 = this.player2.cards.length
   this.player1.cardValue = 0;
@@ -57,84 +58,91 @@ Interface.prototype.cardValue = function () {
   this.over21();
 };
 
-Interface.prototype.selectCard = function () {
+Interface.prototype.selectCard = function() {
   let length = this.deckArray.length
-  let random = Math.floor(Math.random()*length)
+  let random = Math.floor(Math.random() * length)
   return random
 };
 
 Interface.prototype.hit = function() {
   if (this.player1.turn) {
     player = 'player1'
-  }else if(this.player2.turn) {
+  } else if (this.player2.turn) {
     player = 'player2'
-  }else {
+  } else {
     return false;
   }
   let random = this.selectCard()
   this[player].cards.push(this.deckArray[random])
-  this.deckArray.splice(random,1)
-  // should not be this methods job
-  // //changes the turn of the player
-  // this.player1.turn = !this.player1.turn
-  // this.player2.turn = !this.player2.turn
+  this.deckArray.splice(random, 1)
   this.cardValue()
   this.displayCard(player)
   this.whoWon()
+  this.displayCard(player)
 };
 
-Interface.prototype.displayCard = function (player) {
+Interface.prototype.displayCard = function(player) {
   if (player === "player1") {
     document.querySelector('#player1').innerHTML = this.makeDom(player)
-  }else  {
+  } else {
     //do display second card faced down
     //this works but it is so ugly
-    // console.log(document.querySelector("#player2").childNodes.length);
-    if (this.player2.cards.length === 2 && document.querySelector("#player2").childNodes.length < 6 && document.querySelector("#player2").childNodes.length > 2) {
-      document.querySelector('#player2').innerHTML = "<span class='cards "+this.player2.cards[0].suit+"'>"+ this.player2.cards[0].unicode +"</span><span class='cards'>"+ unicode.misc +"</span>"
-    }else{
-    document.querySelector('#player2').innerHTML = this.makeDom(player)
+    if (this.player1.won === false && this.player2.won === false && this.show === false) {
+      console.log("don't display cards");
+      console.log(this.player1.won);
+      console.log(this.player2.won);
+      console.log(this.show);
+      document.querySelector('#player2').innerHTML = `<img class='cards ${this.player2.cards[0].suit}' src="${this.player2.cards[0].unicode}">` + `<img class='cards ${this.player2.cards[0].suit}' src="${unicode.misc}">` + " " + "<span class='block'>Dealers Bank</span><span class='value block'>" + "  " + this.player2.money + "</span>"
+    } else {
+      console.log("display cards");
+      console.log(this.player1.won);
+      console.log(this.player2.won);
+      console.log(this.show);
+
+      document.querySelector('#player2').innerHTML = this.makeDom(player)
     }
     //need to fix the above code
   }
 };
+
 //makes the DOM elememt that get appended into the DOM
-Interface.prototype.makeDom = function (player) {
-  // console.log(player);
-  let domElem =""
+Interface.prototype.makeDom = function(player) {
+  let domElem = ""
   for (let i = 0; i < this[player].cards.length; i++) {
-    domElem += "<span class='cards "+this[player].cards[i].suit+"'>"+ this[player].cards[i].unicode +"</span>"
+
+    domElem += `<img class='cards ${this[player].cards[i].suit}' src="${this[player].cards[i].unicode}">`
   }
-  domElem += "<span class='value'>"+this[player].cardValue+"</span>"
-            + "<br><span>bank</span><span class='value'>"+"  "+this[player].money+"</span>"
+  domElem += "<span class='value block'>" + this[player].cardValue + "</span>" + " " + "<span class='block'>Your Bank</span><span class='value'>" + "  " + this[player].money + "</span>"
   return domElem
 };
 
-Interface.prototype.stay = function () {
+Interface.prototype.stay = function() {
   if (this.player1.turn) {
     // console.log("player 1 turn");
     this.player1.turn = !this.player1.turn
     this.player2.turn = !this.player2.turn
     this.displayCard(player)
     this.ai()
-  }else if (this.player2.turn) {
+  } else if (this.player2.turn) {
     // console.log("player 2");
     this.over21()
     this.comparePoints()
+    //maybe take this out
     this.whoWon()
+    this.displayCard(player)
   }
 };
 
-Interface.prototype.isOver21WithAce = function () {
-  if (this.player1.cardValue > 21 ) {
+Interface.prototype.isOver21WithAce = function() {
+  if (this.player1.cardValue > 21) {
     this.changeAce("player1")
   }
-  if (this.player2.cardValue > 21 ) {
+  if (this.player2.cardValue > 21) {
     this.changeAce("player2")
   }
 };
 
-Interface.prototype.changeAce = function (player) {
+Interface.prototype.changeAce = function(player) {
   //iterate over cards
   for (var i = 0; i < this[player].cards.length; i++) {
     if (this[player].cards[i].name === "ace") {
@@ -148,14 +156,14 @@ Interface.prototype.changeAce = function (player) {
   }
 };
 
-Interface.prototype.over21 = function () {
-  if (this.player1.cardValue > 21 ) {
+Interface.prototype.over21 = function() {
+  if (this.player1.cardValue > 21) {
     console.log("player 1 busted");
     this.player1.turn = false;
     this.player2.turn = false;
     this.player2.won = true;
   }
-  if (this.player2.cardValue > 21 ) {
+  if (this.player2.cardValue > 21) {
     console.log("player 2 busted");
     this.player1.turn = false;
     this.player2.turn = false;
@@ -163,34 +171,45 @@ Interface.prototype.over21 = function () {
   }
 };
 //if both players stay compare the points
-Interface.prototype.comparePoints = function () {
+Interface.prototype.comparePoints = function() {
+  console.log(this.player1.cardValue);
+  console.log(this.player2.cardValue);
   if (this.player1.cardValue > this.player2.cardValue) {
     this.player1.won = true;
-  }else {
+    this.show = true;
+  } else if (this.player1.cardValue < this.player2.cardValue) {
     this.player2.won = true;
+    this.show = true;
+  } else {
+    this.player1.won = false;
+    this.player1.won = false;
+    this.show = true;
   }
 }
 //retrieves the value from the DOM. there is no check yet to make sure it is a numeric value
-Interface.prototype.placeBet = function () {
+Interface.prototype.placeBet = function() {
   let bet = document.getElementById("bet").elements[0].value;
   this.bet = parseInt(bet);
-  document.querySelector('#current').innerHTML = "<p id='current'>The current bet is $"+this.bet+"</p>"
+  document.querySelector('#current').innerHTML = "<p id='current'>The current bet is $" + this.bet + "</p>"
 };
 //takes bet and redistrubtes it to who ever wins
-Interface.prototype.whoWon = function () {
+Interface.prototype.whoWon = function() {
   if (this.player1.won) {
     this.player1.money += this.bet
     this.player2.money -= this.bet
-    document.querySelector('#header').innerHTML = "<p>Player 1 won</p><p>Play another hand</p>"
-  }
-  if (this.player2.won) {
+    document.querySelector('#header').innerHTML = "<span>Player 1 won</span>  <span>Play another hand</span>"
+  } else if (this.player2.won) {
     this.player1.money -= this.bet
     this.player2.money += this.bet
-    document.querySelector('#header').innerHTML = "<p>Player 2 won</p><p>Play another hand</p>"
+    document.querySelector('#header').innerHTML = "<span>Player 2 won</span>  <span>Play another hand</span>"
+    this.show = true
+  } else {
+    this.player1.money = this.player1.money
+    this.player2.money = this.player2.money
   }
 };
 //reset certain information to start a new hand
-Interface.prototype.nextHand = function () {
+Interface.prototype.nextHand = function() {
   this.deckArray = [];
   this.player1.cardValue = 0;
   this.player1.cards = [];
@@ -200,24 +219,26 @@ Interface.prototype.nextHand = function () {
   this.player2.won = false;
   this.player1.turn = true;
   this.player2.turn = false;
+  this.show = false;
   //set aces that have their vaule changed back to 11
   for (var j = 0; j <= 3; j++) {
-    console.log(this.deck[suite[j]].ace.value);
     this.deck[suite[j]].ace.value = 11
   };
   document.querySelector('#header').innerHTML = "Black Jack"
   i.shuffle()
 };
 
-Interface.prototype.ai = function () {
+Interface.prototype.ai = function() {
+  // this.displayCard("player2")
   if (this.player2.cardValue < 17) {
     this.displayCard("player2")
     this.hit("player2")
     this.ai()
-  }else {
+  } else {
     this.stay()
   }
   this.comparePoints()
+  this.displayCard("player2")
 };
 
 i.shuffle()
--- js/cardUnicode.js
+++ js/cardUnicode.js
@@ -1,8 +1,74 @@
 //what is displayed on the DOM can be changed to <img> is different look is desired
 let unicode = {
-  heart:    ['🂱','🂲','🂳','🂴','🂵','🂶','🂷','🂸','🂹','🂺','🂻','🂽','🂾'],
-  spades:   ['🂡','🂢','🂣','🂤','🂥','🂦','🂧','🂨','🂩','🂪','🂫','🂭','🂮'],
-  clubs:    ['🃑','🃒','🃓','🃔','🃕','🃖','🃗','🃘','🃙','🃚','🃛','🃝','🃞'],
-  diamonds: ['🃁','🃂','🃃','🃄','🃅','🃆','🃇','🃈','🃉','🃊','🃋','🃍','🃎'],
-  misc:['🂠']
+  heart:    [
+    'png/ace_of_hearts.png',
+    'png/2_of_hearts.png',
+    'png/3_of_hearts.png',
+    'png/4_of_hearts.png',
+    'png/5_of_hearts.png',
+    'png/6_of_hearts.png',
+    'png/7_of_hearts.png',
+    'png/8_of_hearts.png',
+    'png/9_of_hearts.png',
+    'png/10_of_hearts.png',
+    'png/jack_of_hearts.png',
+    'png/queen_of_hearts.png',
+    'png/king_of_hearts.png'
+  ],
+  spades:   [
+    'png/ace_of_spades.png',
+    'png/2_of_spades.png',
+    'png/3_of_spades.png',
+    'png/4_of_spades.png',
+    'png/5_of_spades.png',
+    'png/6_of_spades.png',
+    'png/7_of_spades.png',
+    'png/8_of_spades.png',
+    'png/9_of_spades.png',
+    'png/10_of_spades.png',
+    'png/jack_of_spades.png',
+    'png/queen_of_spades.png',
+    'png/king_of_spades.png'
+  ],
+  clubs:    [
+    'png/ace_of_clubs.png',
+    'png/2_of_clubs.png',
+    'png/3_of_clubs.png',
+    'png/4_of_clubs.png',
+    'png/5_of_clubs.png',
+    'png/6_of_clubs.png',
+    'png/7_of_clubs.png',
+    'png/8_of_clubs.png',
+    'png/9_of_clubs.png',
+    'png/10_of_clubs.png',
+    'png/jack_of_clubs.png',
+    'png/queen_of_clubs.png',
+    'png/king_of_clubs.png'
+  ],
+  diamonds: [
+    'png/ace_of_diamonds.png',
+    'png/2_of_diamonds.png',
+    'png/3_of_diamonds.png',
+    'png/4_of_diamonds.png',
+    'png/5_of_diamonds.png',
+    'png/6_of_diamonds.png',
+    'png/7_of_diamonds.png',
+    'png/8_of_diamonds.png',
+    'png/9_of_diamonds.png',
+    'png/10_of_diamonds.png',
+    'png/jack_of_diamonds.png',
+    'png/queen_of_diamonds.png',
+    'png/king_of_diamonds.png'
+  ],
+  misc:[
+    'png/back.png'
+  ]
 }
+
+// let unicode = {
+//   heart:    ['🂱','🂲','🂳','🂴','🂵','🂶','🂷','🂸','🂹','🂺','🂻','🂽','🂾'],
+//   spades:   ['🂡','🂢','🂣','🂤','🂥','🂦','🂧','🂨','🂩','🂪','🂫','🂭','🂮'],
+//   clubs:    ['🃑','','🃓','🃔','🃕','🃖','🃗','🃘','🃙','🃚','🃛','🃝','🃞'],
+//   diamonds: ['🃁','🃂','🃃','🃄','🃅','🃆','🃇','🃈','🃉','🃊','🃋','🃍','🃎'],
+//   misc:['🂠']
+// }
